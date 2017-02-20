11b1fd6b63b2ff5932ac59b66e0d04c747eb37a7
Exactimum <exactimum@gmail.com>
2017-02-20 08:16:31 -0800

    Automatically uncompress data, handle missing files

--- download_data.py
+++ download_data.py
@@ -1,15 +1,17 @@
 import urllib.request
+import bz2
 
 base = "http://files.pushshift.io/reddit/"
-categories = ["comments/RC_", "submissions/RS_"]
+categories = ["submissions/RS_", "comments/RC_"]
 
 for cat in categories:
-    year = 2008
-    month = 7
+    year = 2005
+    month = 12
 
-    while year*100+month < 201502:
-        file_name = cat+str(year)+"-"+"0"*(2-len(str(month)))+str(month)+".bz2"
-        url = base+file_name
+    while year*100+month < 200701:
+        file_name = cat+str(year)+"-"+"0"*(2-len(str(month)))+str(month)
+        url = base+file_name+".bz2"
+        print(url)
         month += 1
         if month > 12:
             month = 1
@@ -17,6 +19,10 @@ for cat in categories:
 
 
         # Download the file from `url` and save it locally under `file_name`:
-        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+        with urllib.request.urlopen(url) as response:
             data = response.read() # a `bytes` object
-            out_file.write(data)
+            with open(file_name, 'wb') as out_file:
+                try:
+                    out_file.write(bz2.decompress(data))
+                except OSError:
+                    print("No file found probably, writing empty file instead.")
