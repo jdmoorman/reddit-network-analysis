295c8be250d6a0c1150cfefef3ac515c5132f10b
Exactimum <exactimum@gmail.com>
2017-02-19 23:42:46 -0800

    change directory structure to support addition of script for downloading data automatically from pushshift.io

---
+++ download_data.py
@@ -0,0 +1,22 @@
+import urllib.request
+
+base = "http://files.pushshift.io/reddit/"
+categories = ["comments/RC_", "submissions/RS_"]
+
+for cat in categories:
+    year = 2005
+    month = 12
+
+    while year*100+month < 201002:
+        file_name = cat+str(year)+"-"+"0"*(2-len(str(month)))+str(month)+".bz2"
+        url = base+file_name
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
+
+
+        # Download the file from `url` and save it locally under `file_name`:
+        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+            data = response.read() # a `bytes` object
+            out_file.write(data)
--- paths.py
+++ paths.py
@@ -1,10 +1,14 @@
+# Your personal path to data should go here
 def get_data_directory_path():
-    return "C:\\Users\\jdmoorman\Desktop\\2015_reddit_comments_corpus\\reddit_data\\"
+    return ".\\comments\\"
 
 
+# At some point in the future this list should be replaced with a function that iterates
+# over the contents of the data directory. For now use it to test on small pieces of the data.
 def get_data_file_names():
-    return ["RC_2007-10", "RC_2007-11", "RC_2007-12"]
+    return ["RC_2005-12", "RC_2006-01", "RC_2006-02"]
 
 
+# This path is relative so should never need to change as long as you don't mess with the directory structure.
 def get_profanity_file_name():
     return ".\\example_profanity_map.json"
