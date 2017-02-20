437c616f088e426e4fe3bf3931471b907d9d5934
Exactimum <exactimum@gmail.com>
2017-02-19 22:38:10 -0800

    Store all paths in one place for easy configuration to new user

---
+++ paths.py
@@ -0,0 +1,10 @@
+def get_data_directory_path():
+    return "C:\\Users\\jdmoorman\Desktop\\2015_reddit_comments_corpus\\reddit_data\\"
+
+
+def get_data_file_names():
+    return ["RC_2007-10", "RC_2007-11", "RC_2007-12"]
+
+
+def get_profanity_file_name():
+    return ".\\example_profanity_map.json"
