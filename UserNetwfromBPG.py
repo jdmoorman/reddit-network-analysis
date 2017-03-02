853374359213578363dea183a8fcf2af74fa54b8
jramirezpr <the_passenger89@hotmail.com>
2017-03-01 18:02:57 -0800

    Add files via upload

---
+++ UserNetwfromBPG.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 26 14:02:17 2017
+
+@author: the_p
+"""
+import networkx as nx
+import MultiGraphToWeighted
+x=largest_component
+import bipartite_graphs
+[author,threads]=bipartite_graphs.get_author_and_thread_nodes(x)
+Unigraph=MultiGraphToWeighted.MultToW(x)
+#weighted grap
+from networkx.algorithms import bipartite
+userProjG= bipartite.projected_graph(Unigraph,author)
+betwCent=nx.algorithms.centrality.betweenness_centrality(userProjG,weight='weight')
+degreeCent=nx.algorithms.centrality.degree_centrality(userProjG)
+#no weights, find how to do with weights
+maximum = max(degreeCent, key=degreeCent.get)
+print(maximum)
+import operator
+sorted_degreeCent = sorted(degreeCent.items(), key=operator.itemgetter(1),reverse=True)
+sorted_betwCent=sorted(betwCent.items(),key=operator.itemgetter(1),reverse=True)
+for i in range(10):
+    print(sorted_degreeCent[i])
+file = open("./sortresultsdegreeCent.txt", "w")
+file.write("degreeCent\n")
+
+for i in  range(100):
+    s = str(sorted_degreeCent[i])
+    file.write(s)
+    file.write("\n")
+file.close()
+file = open("./sortresultsbetwCent.txt", "w")
+file.write("betweenness_cent\n")
+for i in range(100):
+    s=str(sorted_betwCent[i])
+    file.write(s)
+    file.write("\n")
+file.close()
