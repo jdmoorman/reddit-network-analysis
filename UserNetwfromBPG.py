7425fc996ef1e0054c0e62cb0f39b6b07dcfdf9e
jramirezpr <the_passenger89@hotmail.com>
2017-03-01 18:52:25 -0800

    Best partition UserGraph(code+plot)
    louvain, on projected graph with multiedges added up( missing projected weighted graph)

--- UserNetwfromBPG.py
+++ UserNetwfromBPG.py
@@ -38,3 +38,19 @@ for i in range(100):
     file.write(s)
     file.write("\n")
 file.close()
+import community
+import matplotlib.pyplot as plt
+partition = community.best_partition(userProjG)
+size = float(len(set(partition.values())))
+pos = nx.spring_layout(userProjG)
+count = 0.
+for com in set(partition.values()) :
+    count = count + 1.
+    list_nodes = [nodes for nodes in partition.keys()
+                                if partition[nodes] == com]
+    nx.draw_networkx_nodes(userProjG, pos, list_nodes, node_size = 20,
+                                node_color = str(count / size))
+
+
+nx.draw_networkx_edges(G,pos, alpha=0.5)
+plt.show()
