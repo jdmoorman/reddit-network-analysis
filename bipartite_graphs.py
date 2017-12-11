f4ac1a8972acf636a94a8a9d0f0be66bcc1a7372
Jacob Moorman <jacob@s-149-142-31-44.resnet.ucla.edu>
2017-12-10 17:47:54 -0800

    process some data, add some paths for the results directories

--- bipartite_graphs.py
+++ bipartite_graphs.py
@@ -49,6 +49,14 @@ def files_to_polyad_counts(iter_json=None, count_id="count",
 
     return polyad_df
 
+def user_thread_edgecounts(**kwargs):
+    kwargs["vert_keys"] = ["link_id", "author"]
+    return files_to_polyad_counts(**kwargs)
+
+def user_subreddit_edgecounts(**kwargs):
+    kwargs["vert_keys"] = ["subreddit", "author"]
+    return files_to_polyad_counts(**kwargs)
+
 
 if __name__ == "__main__":
     """
@@ -57,14 +65,44 @@ if __name__ == "__main__":
     """
 
     # temporary sandbox for testing
-    from iterators import list_files_from_date_range, iter_multiple_json_files
-    from data.data_paths import LOCAL_COMMENTS_FMT_STR
+    from iterators import list_files_from_date_range, \
+        iter_multiple_json_files, iter_single_json_file
+    from data.data_paths import LOCAL_COMMENTS_FMT_STR, THREAD_SR_FMT_STR, \
+        SR_FMT_STR
     from sys import argv
+    from os.path import join
+
+    start_year, start_month, end_year, end_month = (
+        int(argv[1]), int(argv[2]), int(argv[3]), int(argv[4]))
 
     comment_files = list_files_from_date_range(
         LOCAL_COMMENTS_FMT_STR,
-        int(argv[1]), int(argv[2]), int(argv[3]), int(argv[4]))
+        start_year, start_month, end_year, end_month)
 
+    thread_out_files = list_files_from_date_range(
+        THREAD_SR_FMT_STR,
+        start_year, start_month, end_year, end_month)
+
+    subreddit_out_files = list_files_from_date_range(
+        SR_FMT_STR,
+        start_year, start_month, end_year, end_month)
+
+    for comment_filepath, thread_out_filepath, subreddit_out_filepath in \
+            zip(comment_files, thread_out_files, subreddit_out_files):
+
+        print(comment_filepath, thread_out_filepath, subreddit_out_filepath)
+
+        threads_df = files_to_polyad_counts(
+            vert_keys=["subreddit", "link_id"],
+            iter_json=iter_single_json_file(comment_filepath))
+        subreddit_df = files_to_polyad_counts(
+            vert_keys=["subreddit"],
+            iter_json=iter_single_json_file(comment_filepath))
+
+        threads_df.to_csv(thread_out_filepath)
+        subreddit_df.to_csv(subreddit_out_filepath)
+
+    exit()
     edge_df = files_to_polyad_counts(
         iter_json=iter_multiple_json_files(comment_files),
         vert_keys=["author", "link_id"])
--- data/data_paths.py
+++ data/data_paths.py
@@ -6,3 +6,8 @@ REMOTE_THREADS_FMT_STR = join(REMOTE_BASE, "submissions", "RS_{}.bz2")
 
 LOCAL_COMMENTS_FMT_STR = join(LOCAL_BASE, "comments", "RC_{}.json")
 LOCAL_THREADS_FMT_STR = join(LOCAL_BASE, "submissions", "RS_{}.json")
+
+SR_EDGES_FMT_STR = join(LOCAL_BASE, "user_subreddit_edgecounts", "{}.csv")
+THREAD_EDGES_FMT_STR = join(LOCAL_BASE, "user_thread_edgecounts", "{}.csv")
+THREAD_SR_FMT_STR = join(LOCAL_BASE, "thread_subreddit_edgecounts", "{}.csv")
+SR_FMT_STR = join(LOCAL_BASE, "subreddit_counts", "{}.csv")
